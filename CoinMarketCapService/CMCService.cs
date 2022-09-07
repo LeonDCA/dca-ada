16f08c8435618d4619df62aab65035d0e021d7a7
Leon <leon-psv@hotmail.com>
2022-09-07 21:25:16 +0200

    Add project files.

---
+++ CoinMarketCapService/CMCService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Reflection.PortableExecutable;
+using System.Web;
+
+namespace CoinMarketCapService
+{
+    public class CMCService
+    {
+        private static string API_KEY = "5c70d535-3c51-47a1-b6a3-40aef8fbf5e3";
+        private readonly HttpClient httpClient;
+
+        public CMCService()
+        {
+            this.httpClient = new HttpClient();
+        }
+
+        public async Task<string> GetData()
+        {
+            var url = new UriBuilder("https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest");
+
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+            queryString["start"] = "1";
+            queryString["limit"] = "5000";
+            queryString["convert"] = "USD";
+
+            url.Query = queryString.ToString();
+
+            using (var request = new HttpRequestMessage(HttpMethod.Get, url.ToString()))
+            {
+                request.Headers.Add("X-CMC_PRO_API_KEY", API_KEY);
+                request.Headers.Add("Accepts", "application/json");
+
+                HttpResponseMessage response = await httpClient.SendAsync(request);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    return "Internal server Error";
+                }
+            }
+        }
+    }
+}
---
+++ dca-ada/Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using CoinMarketCapService;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dca_ada.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async void OnGet()
+        {
+            CMCService cMCService = new CMCService();
+            var data = await cMCService.GetData();
+        }
+    }
+}
